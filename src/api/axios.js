9bfa3c24dba2c66fecc40c7435842edaab0236e4
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-09-03 21:40:23 +0900

    feat : feat axios interceptor #52

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,4 +4,24 @@ const instance = axios.create({
   baseURL: "http://localhost:3001",
 });
 
+instance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    console.log("[DEBUG] request error : ", error);
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    console.log("[DEBUG] response error : ", error);
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
